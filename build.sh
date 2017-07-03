a7cb1782b42d0d8ebaadcd8d87f1391e4a13bc15
Ying Huang <yhuang@imvu.com>
2017-07-02 18:01:14 -0700

    n

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 cd src/
-./configure --with-features=huge --enable-multibyte --enable-perlinterp --enable-rubyinterp --enable-tclinterp --enable-cscope --enable-pythoninterp \
+./configure --with-features=huge --enable-multibyte --enable-perlinterp --enable-rubyinterp --enable-tclinterp --with-tlib=ncurses --enable-cscope --enable-pythoninterp \
     && make \
     && cd - \
     && ~/loadrc/gitrc/gci.sh
