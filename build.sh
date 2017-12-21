6a6404e8734451fb503cacb09db7718cbaf6c355
Ying Huang <yhuang@imvu.com>
2017-12-20 18:16:06 -0800

    n

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
+apt-get build-dep -y vim-gnome
 cd src/
-./configure --with-features=huge --enable-multibyte --enable-perlinterp --enable-rubyinterp --enable-tclinterp --with-tlib=ncurses --enable-cscope --enable-pythoninterp \
+./configure --with-features=huge --enable-multibyte --enable-perlinterp --enable-rubyinterp --enable-tclinterp --enable-cscope --enable-pythoninterp \
     && make \
     && cd - \
     && ~/loadrc/gitrc/gci.sh
